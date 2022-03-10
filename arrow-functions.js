e6b1e2522abfa9ed21d33a58ad335f2592f93794
Coder731 <stephenobrien1@gmail.com>
2022-03-10 11:07:45 +0000

    Set up arrow functions js

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,83 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6,4);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
+
+
+// PART 2:
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+// average
+// averageMarks = sum / numParticipants
+// const avgMrks = 
+const averagePoints = (arr, subject) => {
+    return 
+};
+// Array.prototype.students = function() {
+//   var sum = 0;
+//   var count = this.length;
+//   for (i=0; i<count; i++) {
+//     sum += this[i];
+//   }
+//   return sum/count;
+// }
+// https://stackoverflow.com/questions/20905354/javascript-calculating-the-average-of-an-array-using-for-loop
+
+
+let result =  averagePoints (students,'english');
+console.log(result)
+console.log(students.subjects)
