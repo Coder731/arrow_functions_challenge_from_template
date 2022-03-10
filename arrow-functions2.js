29cba830c784c41f43554b4b254dd18fa3969cd8
Coder731 <stephenobrien1@gmail.com>
2022-03-10 11:17:06 +0000

    Copy pseudocode from README to js file Start input

--- arrow-functions2.js
+++ arrow-functions2.js
@@ -39,3 +39,32 @@ const averagePoints = (arr, subject) => {
 let result =  averagePoints (students,'english');
 console.log(result)
 console.log(students.subjects)
+
+// Part 3 / Attempt 2:
+
+
+//define input function:
+// Reference:
+// https://www.mikedane.com/web-development/javascript/getting-user-input/#:~:text=In%20JavaScript%2C%20we%20can%20get%20user%20input%20like%20this%3A&text=var%20name%20%3D%20window.,out%20what%20they%20entered%20in.
+
+var name = window.prompt("Enter your name: ");
+alert("Your name is " + name);
+
+
+// So it takes input from user
+let input = input();
+// passes it into program
+
+// uses the key
+
+// to get the value
+
+// and console.log(value)
+
+// if (without if) 
+// if user gives english
+// gets 78.5
+
+// as this is average english score for all students
+
+// 3 students
