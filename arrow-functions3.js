68f62540e737832614413302d6e3876f462be414
Coder731 <stephenobrien1@gmail.com>
2022-03-11 01:18:21 +0000

    Copy new plan as pseudo code in comments
    into 3rd js file
    from README

---
+++ arrow-functions3.js
@@ -0,0 +1,22 @@
+// ## New approach: simple, non gerneral, specific:
+// ### Step 1:
+// take user input, using previously done code
+// ->  tidy it up
+//     so it takes
+//     english
+// ->  and passes stuff along
+// ->  and outputs 77.5
+
+// ### Step 2:
+// manual way to do just
+// the addition, not the average yet.... is:
+
+// ### Step 3: as an interim / prelude stepL: (declare variables) :
+// a = students.key  -> value[0]
+// b = students.key  -> value[1]
+// c = students.key  -> value[2]
+
+// ### Step 4: 
+// take sum
+// divide by 3
+// = average (avg)
