8ec887beb331b3866c2568ac52022d1f4bc18d26
Coder731 <stephenobrien1@gmail.com>
2022-03-11 22:37:11 +0000

    Cut comments from js file 1

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,13 +1,7 @@
-// Arrow Function With Parameters
-const addTwoNumbers = (a, b) => {
-    // Code block
-    return a + b;
-}
+const addTwoNumbers = (a, b) => a + b;
 let sum = addTwoNumbers(3, 5);
 console.log(sum);
 
-// Single Line Arrow Function With Parameters
-// const addTwoNumbers2 = (a, b) => (a + b); also valid
 const addTwoNumbers2 = (a, b) => a + b;
 let sum2 = addTwoNumbers2(6,4);
 console.log(sum2);
