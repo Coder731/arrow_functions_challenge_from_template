c0cefa18b1beecdeaab1d870171db8820aa8fc63
Coder731 <stephenobrien1@gmail.com>
2022-03-11 01:24:23 +0000

    Copy code for taking input from 2 js to 3 js file

--- arrow-functions3.js
+++ arrow-functions3.js
@@ -1,6 +1,34 @@
 // ## New approach: simple, non gerneral, specific:
 // ### Step 1:
-// take user input, using previously done code
+// take user input, using previously done code:
+
+    // So it takes input from user
+
+        //define input function:
+        // Reference:
+        // https://www.mikedane.com/web-development/javascript/getting-user-input/#:~:text=In%20JavaScript%2C%20we%20can%20get%20user%20input%20like%20this%3A&text=var%20name%20%3D%20window.,out%20what%20they%20entered%20in.
+
+        // prompt 2.0 (see Debug Issue 2 in README)
+        // https://nodejs.org/en/knowledge/command-line/how-to-prompt-for-command-line-input/
+        import { createInterface } from 'readline';
+        const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout
+        });
+
+        rl.question('What is your name ? ', function (name) {
+        rl.question('Where do you live ? ', function (country) {
+            console.log(`${name}, is a citizen of ${country}`);
+            rl.close();
+        });
+        });
+
+        rl.on('close', function () {
+        console.log('\nBYE BYE !!!');
+        process.exit(0);
+        });
+
+
 // ->  tidy it up
 //     so it takes
 //     english
