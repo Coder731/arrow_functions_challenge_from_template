c00c310ea09f01c94a1751982883f46ce370c1c2
Coder731 <stephenobrien1@gmail.com>
2022-03-11 22:35:25 +0000

    Cut comments from js file 1

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,16 +1,3 @@
-/**
- * To run this file in Gitpod, use the 
- * command node arrow-functions.js in the terminal
- */
-
-// Vanilla JavaScript Function
-// function addTwoNumbers(a, b) {
-//     // Code block
-//     return a + b;
-// }
-// let sum = addTwoNumbers(3, 5);
-// console.log(sum);
-
 // Arrow Function With Parameters
 const addTwoNumbers = (a, b) => {
     // Code block
