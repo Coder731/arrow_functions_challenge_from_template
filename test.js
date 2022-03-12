954f1b37b9286c54f4fbac8b144fdc0896c3399e
Coder731 <stephenobrien1@gmail.com>
2022-03-12 01:43:52 +0000

    Add test js fille

---
+++ test.js
@@ -0,0 +1,8 @@
+// https://betterprogramming.pub/accessing-an-objects-keys-values-and-entries-in-javascript-e7bb5d33d11c
+
+const hero = {
+    name: 'Batman',
+    city: 'Gotham'  
+  };
+  let a = Object.keys(hero); // => ['name', 'city']
+  console.log(a)
