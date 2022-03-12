11fe6568892f107311721484b8489fa1ef53e3a3
Coder731 <stephenobrien1@gmail.com>
2022-03-12 15:24:36 +0000

    Debug Add students array to current loop file

--- loop/loop-object-in-array-v3.js
+++ loop/loop-object-in-array-v3.js
@@ -22,6 +22,28 @@ var cars = [
     }
 ]
 
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+
 // loop 1 variable
 for(i = 0; i < cars.length; i++) {
     if(cars[i].year < 2010) {
