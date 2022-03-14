144a448343a722b01638ebba8e489132114f11d5
Coder731 <stephenobrien1@gmail.com>
2022-03-14 13:58:11 +0000

    Bug Add variable b to strToVar js output value
    output of
    b
    is
    undefined

--- strToVar.js
+++ strToVar.js
@@ -1,7 +1,10 @@
+let b;
+
 // convert string to variable name
                         // https://stackoverflow.com/questions/5613834/convert-string-to-variable-name-in-javascript
                         function SetToString(varString) {
                             var newValue = "string";
-                            eval(varString + " = " + "'" + newValue + "'");
+                            b = eval(varString + " = " + "'" + newValue + "'");
                           }                          
                         // end converter
+                        console.log(b)
