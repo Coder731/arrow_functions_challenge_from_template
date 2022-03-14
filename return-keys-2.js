2dd33ec388e7034100a6fa7317809049b4e26bcb
Coder731 <stephenobrien1@gmail.com>
2022-03-14 04:20:09 +0000

    Duplicate return keys js Add user input toVersion2

--- return-keys-2.js
+++ return-keys-2.js
@@ -23,6 +23,24 @@ let a = students;
 
 
 
+// https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  
+  readline.question(`What subject are you interested in?`, subject => {
+    console.log(`The subject you have selected is: ${subject}!`)
+    readline.close()
+  })
+  
+// pseudocode:
+    // if input == english{
+    //     // set selector to english
+    // }
+
+
     console.log(`${students[0].results.english}`);
     
     for(var i in students){
