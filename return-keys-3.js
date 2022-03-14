22f860b8d07df3089b16f522ba0178aa2219f8c2
Coder731 <stephenobrien1@gmail.com>
2022-03-14 04:42:19 +0000

    format

--- return-keys-3.js
+++ return-keys-3.js
@@ -34,8 +34,8 @@ const readline = require('readline').createInterface({
     console.log(`The subject you have selected is: ${subject}!`)
     readline.close()
   })
-  
-// Next line causes subject variable scope bug
+
+// Next line causes subject variable scope bug:
 //   console.log(`subject outside block scope: ${subject}`)
 
 // pseudocode:
