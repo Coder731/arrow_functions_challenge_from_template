2579f8f525187cdfc6959bd69dd1fb9b5a80c480
Coder731 <stephenobrien1@gmail.com>
2022-03-14 17:48:39 +0000

    Minimum output version just gives avg in sentence

--- return-keys-13.js
+++ return-keys-13.js
@@ -23,11 +23,10 @@ array = []
 
 for(var arrayStudentCount in students) { // counts in students array : 0-2 
     for(var jthPropertyInStudentsI in students[arrayStudentCount]) {  // counts properties 0-3
-        for(var studentObjPropValIsKeyValPair in students[arrayStudentCount][jthPropertyInStudentsI]) { /* counts inside properties, gives first high level value currently on */ console.log(`students[arrayStudentCount][jthPropertyInStudentsI] is : ${students[arrayStudentCount][jthPropertyInStudentsI]}`);
-            var sub_key2 = studentObjPropValIsKeyValPair; console.log(`studentObjPropValIsKeyValPair is : ${studentObjPropValIsKeyValPair}`) // counts the number the letter on the next line is in a sequence
+        for(var studentObjPropValIsKeyValPair in students[arrayStudentCount][jthPropertyInStudentsI]) { /* counts inside properties, gives first high level value currently on */
+            var sub_key2 = studentObjPropValIsKeyValPair; // counts the number the letter on the next line is in a sequence
             var sub_val2 = students[arrayStudentCount][jthPropertyInStudentsI][studentObjPropValIsKeyValPair]; // this is actually counting through the letters in the name 'John' with each letter at a different count 
-            if (jthPropertyInStudentsI == "results") { console.log(`studentObjPropValIsKeyValPair is : ${jthPropertyInStudentsI}`) // gives name of subject
-                // console.log(studentObjPropValIsKeyValPair); // logs subjects also
+            if (jthPropertyInStudentsI == "results") { // gives name of subject
                 if (studentObjPropValIsKeyValPair == "english") {
                     // convert string to variable name
                     let b;
@@ -38,7 +37,6 @@ for(var arrayStudentCount in students) { // counts in students array : 0-2
                         SetToString();  
                     for (var count in Object.keys(students[arrayStudentCount].results)) {
                         if (Object.keys(students[arrayStudentCount].results)[count] == "english") {
-                            console.log(`students arrayCount jthProperty... valIsKeyValPair is: ${students[arrayStudentCount][jthPropertyInStudentsI][studentObjPropValIsKeyValPair]}`);
                             array.push(students[arrayStudentCount][jthPropertyInStudentsI][studentObjPropValIsKeyValPair]);
 
                         }
@@ -49,25 +47,18 @@ for(var arrayStudentCount in students) { // counts in students array : 0-2
     }
 }
 
-console.log(array);
-
 // get average
-
-for (x in array) {
-    console.log(array[x]);    
-}
-
-// let s=array.sum();
-// console.log(s);
-
-
-// https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
-const sum = [1, 2, 3].reduce((partialSum, a) => partialSum + a, 0);
-console.log(sum); // 6
-
 const sum2 = array.reduce((partialSum, a) => partialSum + a, 0);
-console.log(sum2);
 let number = array.length;
-console.log(`number is : ${number}`);
 let average = sum2 / number;
 console.log(`average result for english is : ${average}`);
+
+//////////////////////////////////////////////////////////////////////
+
+// for REFERENCES etc :
+
+// please SEE REPOSITORY :
+
+// https://github.com/Coder731/arrow_functions_challenge_from_template
+
+//////////////////////////////////////////////////////////////////////
